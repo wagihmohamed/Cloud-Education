dd635df6401500af724b9f02400f2bca280ba20f
shawky  sobhy <shawkysobhy98@gmail.com>
2023-02-24 23:40:42 +0200

    add mockup folder

--- src/screens/LandingPage/index.tsx
+++ src/screens/LandingPage/index.tsx
@@ -13,20 +13,7 @@ import {
     landingimageCaseStudy,
 } from "assets";
 
-const CaseStudyStatments = {
-    castStudy1: {
-        content:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.  ",
-    },
-    castStudy2: {
-        content:
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-    castStudy3: {
-        content:
-            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
-    },
-};
+import { CaseStudyStatments } from "mockup/caseStudyStatments";
 
 export const LandingPage = () => {
     return (
