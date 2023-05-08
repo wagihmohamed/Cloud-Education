09b1b5631c8d6726ed196dd1c6e3c1c95f9ce618
shawky  sobhy <shawkysobhy98@gmail.com>
2023-05-08 15:02:32 +0300

    profile screen responsive

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,24 +1,56 @@
-/* eslint-disable no-console */
 import { useState } from 'react';
-import { Avatar, Box, Typography } from '@mui/material';
+import { Avatar, Box, Typography, styled, useMediaQuery } from '@mui/material';
 import {
 	CustomButton,
 	CustomLayout,
 	EditProfileModal,
 	SettingsModal,
 } from 'components';
-import { Stack, styled } from '@mui/system';
 import { ProfileImg } from 'assets';
 import { UserInfo } from 'models';
 import SettingsIcon from '@mui/icons-material/Settings';
-
+import { theme } from 'theme';
 const InfoLabel = styled(Typography)(() => ({
 	fontWeight: 'bold',
 	fontSize: '1.5rem',
 	marginTop: 5,
 	marginBottom: 5,
 }));
-
+const styles = {
+	settingButton: {
+		mt: 3,
+		mr: 5,
+		width: '150',
+		marginLeft: 'auto',
+		marginTop: '4rem',
+	},
+	infoProfileContainer: {
+		p: '1rem',
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	infoProfil: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'center',
+		border: '3px solid #000',
+		borderRadius: '1rem',
+		width: '80%',
+		alignItems: 'center',
+		px: '1rem',
+	},
+	infoProfileFull: {
+		width: '100%',
+		py: 1.5,
+	},
+	infoProfileMd: {
+		width: '80%',
+		py: 3,
+	},
+	avatarStyle: { width: 170, height: 170, border: '3px solid #000' },
+};
 export const ProfileScreen = () => {
 	const [isEditProfileShown, setIsEditProfileShown] = useState(false);
 	const [isSettingsShown, setIsSettingsShown] = useState(false);
@@ -32,76 +64,43 @@ export const ProfileScreen = () => {
 		currentLevel: 'Level 4 CS',
 		creditHours: '119/141',
 	});
+	const mdScreen = useMediaQuery(theme.breakpoints.down('md'));
 	return (
 		<CustomLayout>
-			<Box
-				sx={{
-					my: 4,
-					mx: 5,
-					height: '100%',
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				<Box
-					sx={{
-						position: 'absolute',
-						top: 0,
-						right: 0,
-						mt: 3,
-						mr: 3,
-						width: 150,
+			<Box sx={styles.settingButton}>
+				<CustomButton
+					onClick={() => {
+						setIsSettingsShown(true);
 					}}
+					px={5}
+					startIcon={<SettingsIcon />}
 				>
-					<CustomButton
-						onClick={() => {
-							setIsSettingsShown(true);
-						}}
-						px={3}
-						startIcon={<SettingsIcon />}
-					>
-						Settings
-					</CustomButton>
-				</Box>
+					Settings
+				</CustomButton>
+			</Box>
+			<Box sx={styles.infoProfileContainer}>
 				<Box
-					sx={{
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						height: '50%',
-						border: '3px solid #000',
-						px: 6,
-						py: 3,
-					}}
+					sx={[
+						styles.infoProfil,
+						mdScreen ? styles.infoProfileFull : styles.infoProfileMd,
+					]}
 				>
-					<Stack
-						flexDirection="row"
-						justifyContent="space-between"
-						alignItems="flex-start"
-						gap="300px"
-					>
-						<Box>
-							<InfoLabel>First Name : {userIfo.firstName}.</InfoLabel>
-							<InfoLabel>Last Name : {userIfo.lastName}.</InfoLabel>
-							<InfoLabel>Phone Number : {userIfo.phoneNumber}.</InfoLabel>
-							<InfoLabel>Email : {userIfo.email}.</InfoLabel>
-							<InfoLabel>GPA : {userIfo.GPA}.</InfoLabel>
-							<InfoLabel>Role : {userIfo.role}.</InfoLabel>
-							<InfoLabel>Current Level : {userIfo.currentLevel}.</InfoLabel>
-							<InfoLabel>Credit Hours : {userIfo.creditHours}.</InfoLabel>
-						</Box>
-						<Avatar
-							src={ProfileImg as string}
-							sx={{ width: 170, height: 170, border: '3px solid #000' }}
-						/>
-					</Stack>
+					<Box sx={mdScreen ? { order: 2 } : { order: 1 }}>
+						<InfoLabel>First Name : {userIfo.firstName}.</InfoLabel>
+						<InfoLabel>Last Name : {userIfo.lastName}.</InfoLabel>
+						<InfoLabel>Phone Number : {userIfo.phoneNumber}.</InfoLabel>
+						<InfoLabel>Email : {userIfo.email}.</InfoLabel>
+						<InfoLabel>GPA : {userIfo.GPA}.</InfoLabel>
+						<InfoLabel>Role : {userIfo.role}.</InfoLabel>
+						<InfoLabel>Current Level : {userIfo.currentLevel}.</InfoLabel>
+						<InfoLabel>Credit Hours : {userIfo.creditHours}.</InfoLabel>
+					</Box>
+					<Avatar src={ProfileImg as string} sx={styles.avatarStyle} />
 					<CustomButton
 						onClick={() => {
 							setIsEditProfileShown(true);
 						}}
-						mt="auto"
+						m="1rem"
 					>
 						Edit Profile
 					</CustomButton>
