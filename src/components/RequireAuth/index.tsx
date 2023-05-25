262637e98ff177712b344f37ffebfa7a8bb7ec47
wagihmohamed <wagihm969@gmail.com>
2023-05-25 13:39:54 +0300

    updated the require auth with the new changes

--- src/components/RequireAuth/index.tsx
+++ src/components/RequireAuth/index.tsx
@@ -3,10 +3,10 @@ import { Navigate, Outlet, useParams } from 'react-router-dom';
 import { useAuth } from 'zustandStore';
 
 export const RequireAuth = () => {
-	const { token } = useAuth();
+	const { token, subDomain } = useAuth();
 	const { organizationId } = useParams();
 	const { organizationName } = useGetOrganizationName();
-	const paramAuth = organizationName || organizationId;
+	const paramAuth = organizationName || organizationId || subDomain;
 	return (
 		<>{token ? <Outlet /> : <Navigate to={`/${paramAuth}/login`} replace />}</>
 	);
